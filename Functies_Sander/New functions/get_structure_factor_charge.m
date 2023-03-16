2c28998c576c7a034bdd760da2578214119b0434
sanderdemeyer <sander.demeyer@ugent.be>
2023-03-16 19:07:05 +0100

    charge struct funct more efficient by joint to separate

--- Functies_Sander/New functions/get_structure_factor_charge.m
+++ Functies_Sander/New functions/get_structure_factor_charge.m
@@ -1,20 +1,23 @@
-function s = get_structure_factor_charge(gs_mps, P, Q, max_dist, symmetries, kwargs)
+function s = get_structure_factor_charge(gs_mps, P, Q, max_dist, symmetries, operator_type, kwargs)
     arguments
         gs_mps
         P
         Q
         max_dist
         symmetries = 'U1_SU2'
+        operator_type = 'separate'
         kwargs.corr = []
         kwargs.convention = 'conventional'
         kwargs.system = {'1D'}
         kwargs.qx_size = 500
         kwargs.tol_check = false
     end
-    warning('This might be more efficient if the 2 number operators are given separately');
+    if strcmp(kwargs.operator_type, 'joint')
+        warning('This might be more efficient if the 2 number operators are given separately');
+    end
 
-    [O_alpha, O_beta] = get_number_number_operators(P, Q, 'convention', kwargs.convention, 'symmetries', symmetries);
+    [O_alpha, O_beta] = get_number_number_operators(P, Q, 'convention', kwargs.convention, 'symmetries', symmetries, 'operator_type', operator_type);
 
     struct = namedargs2cell(kwargs);
-    s = get_structure_factor({O_alpha, O_beta}, gs_mps, max_dist, 'joint', struct{:});
+    s = get_structure_factor({O_alpha, O_beta}, gs_mps, max_dist, operator_type, struct{:});
 end
