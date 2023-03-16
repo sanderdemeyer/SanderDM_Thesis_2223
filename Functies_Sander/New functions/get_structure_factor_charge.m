05209da16b1dc0f7f00abdca37246ad21813654f
sanderdemeyer <sander.demeyer@ugent.be>
2023-03-16 18:50:51 +0100

    little update

--- Functies_Sander/New functions/get_structure_factor_charge.m
+++ Functies_Sander/New functions/get_structure_factor_charge.m
@@ -12,7 +12,7 @@ function s = get_structure_factor_charge(gs_mps, P, Q, max_dist, symmetries, kwa
         kwargs.tol_check = false
     end
     warning('This might be more efficient if the 2 number operators are given separately');
-    
+
     [O_alpha, O_beta] = get_number_number_operators(P, Q, 'convention', kwargs.convention, 'symmetries', symmetries);
 
     struct = namedargs2cell(kwargs);
