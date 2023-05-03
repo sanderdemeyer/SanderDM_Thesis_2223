132e096c3d977ee380cd0b264e87ab27a36bd637
sanderdemeyer <sander.demeyer@ugent.be>
2023-05-03 17:21:04 +0200

    hole_density_operator

--- Functies_Sander/hole_density_operator.m
+++ Functies_Sander/hole_density_operator.m
@@ -1,8 +1,8 @@
-function O = number_operator_None_SU2()
+function O = hole_density_operator()
     % This assumes symmetry None_SU2
 
     pspace = get_spaces_Hubbard_None_SU2();
     tens_one_site = Tensor(pspace, pspace);
 
-    O = fill_tensor(tens_one_site, {reshape([0 0; 0 2], 2, 2), 1});
+    O = fill_tensor(tens_one_site, {reshape([1 0; 0 -1], 2, 2), 0});
 end
